b4bab6d142e1e2f0cf88b26180fab08a052b03d7
2Bleu <83442032+2Bleu@users.noreply.github.com>
2022-02-09 19:42:26 -0500

    3rd commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Load Express module
+const express = require('express');
+
+// Create an Express object
+const app = express();
+const port = 3000;
+
+// Setup the Get Route
+app.get('/', function(req, res) {
+  res.send('Hello World!')
+});
+
+// list for request on port 3000
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
+});
