b8cf930326008897df1b610834a24221a6f95e34
2Bleu <83442032+2Bleu@users.noreply.github.com>
2022-02-14 10:41:01 -0500

    Update 3
    Updated app.js and added course and student js files.

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Setup the Routes
+var course = require("./course.js");
+var student = require("./student.js");
+
 // Setup the Get Route
 app.get('/', function(req, res) {
   res.send('Hello World!')
 });
 
+app.use('/course', course);
+app.use('/student', student);
+
 // list for request on port 3000
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}!`)
 });
+
+console.log('Hiya');
